aa80b5b22a5643237c3b58f3d52916db6b87251e
= <=>
2024-03-31 12:33:08 +0530

    Created Factory generic class with type parameters T and U with necessary constraints

--- FactoryDesignPattern.EmployeeApp/Program.cs
+++ FactoryDesignPattern.EmployeeApp/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace FactoryDesignPattern.EmployeeApp;
+
+interface IEmployee{
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
+
+/*
+ * T - type parameter with rules: must be a class reference type, must inherit "U", must allow creation of "zero" argument object using "zero" argument constructor
+ * U - type parameter with rules: must be a class reference type, must inherit "IEmployee" custom reference type.
+ */
+static class Factory<T,U> where T:class,U,new() where U:class,IEmployee{
+    /*
+    * We know that "T" inherits "U" as we set the constraint
+    * Hence, "U" can be reference type for the object of "T"
+    * Hence, we return "U" which holds the object "new T();"
+    */
+    static U GetInstance(){
+        U u = new T();
+        return u;
+    }
+}
+
+public class Program{
+    public static void Main(string[] args){
+
+    }
+}
